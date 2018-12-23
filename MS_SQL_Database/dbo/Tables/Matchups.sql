e5d36cab9ba254eb0a485a22aefa6bff4ae9d2c7
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2018-12-23 21:29:33 +0100

    3.4 - Dodano powiązania w tabelach

--- MS_SQL_Database/dbo/Tables/Matchups.sql
+++ MS_SQL_Database/dbo/Tables/Matchups.sql
@@ -3,4 +3,5 @@
 	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [WinnerId] INT NOT NULL, 
     [MatchupRound] INT NOT NULL, 
+    CONSTRAINT [FK_Matchups_ToTable] FOREIGN KEY ([WinnerId]) REFERENCES [Teams]([id]), 
 )
--- MS_SQL_Database/dbo/Tables/TournamentPrizes.sql
+++ MS_SQL_Database/dbo/Tables/TournamentPrizes.sql
@@ -3,4 +3,6 @@
 	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [TournamentId] INT NOT NULL, 
     [PrizeId] INT NOT NULL, 
+    CONSTRAINT [FK_TournamentPrizes_ToTable] FOREIGN KEY ([TournamentId]) REFERENCES [Tournaments]([id]), 
+    CONSTRAINT [FK_TournamentPrizes_ToTable_1] FOREIGN KEY ([PrizeId]) REFERENCES [Prizes]([id]), 
 )
