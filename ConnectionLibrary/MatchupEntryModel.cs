9545be24703feb9af8c646d5749e1aa7c49209c0
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2018-12-23 13:57:23 +0100

    L1.2 - Dodano część klas

---
+++ ConnectionLibrary/MatchupEntryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        public double Store { get; set; }
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
